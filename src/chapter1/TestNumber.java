7e0621dc36f224d6b213e56a972c6c432438ad8b
Petr <petrzl@bk.ru>
2019-03-13 12:34:42 +0300

    in TestNumber changed arrays of Numbers to List<Number>

--- src/chapter1/TestNumber.java
+++ src/chapter1/TestNumber.java
@@ -1,7 +1,10 @@
 package chapter1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestNumber {
-    public static double sum(Number[] num) {
+    public static double sum(List<Number> num) {
         double sum = 0.0;
         for (Number n: num) {
             sum += n.doubleValue();
@@ -10,11 +13,11 @@ public class TestNumber {
         return sum;
     }
     public static void main(String[] args) {
-        Number[] nums = new Number[4];
-        nums[0] = new Byte((byte)10);
-        nums[1] = new Integer(10);
-        nums[2] = new Float(10f);
-        nums[3] = new Double(10d);
+        List<Number> nums = new ArrayList<>();
+        nums.add(new Byte((byte)10));
+        nums.add(new Integer(10));
+        nums.add(new Float(10f));
+        nums.add(new Double(10d));
         System.out.println(sum(nums));
     }
 }
